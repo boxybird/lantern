9961f31b5ab32ade6a2a60ee550597fc25778ca7
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-03-11 12:24:12 -0700

    Refactor to subdomain for WP

--- public/cms/wp-content/mu-plugins/lantern-bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!wp_installing()) {
-    define('LANTERN_URL', plugin_dir_url(__FILE__) . 'lantern');
-    define('LANTERN_PATH', plugin_dir_path(__FILE__) . 'lantern');
-
-    require_once __DIR__ . '/lantern/bootstrap.php';
-}
