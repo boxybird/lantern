39e333f11a62bcad5b720f27cc60c4ee56c57b43
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-03-11 12:56:39 -0700

    wip

--- cms.lantern/wp-content/mu-plugins/lantern/src/Setup.php
+++ cms.lantern/wp-content/mu-plugins/lantern/src/Setup.php
@@ -2,10 +2,21 @@
 
 namespace BoxyBird\Lantern;
 
+use Illuminate\Support\Str;
+
 class Setup
 {
     public static function init(): void
     {
-        //
+        add_filter('rest_url', [Setup::class, 'restUrl']);
+    }
+
+    /**
+     * Fixes WordPress REST API issue when
+     * WordPress Address (URL) is not the same as Site Address (URL)
+     */
+    public static function restUrl($url): string
+    {
+        return Str::replace(home_url(), site_url(), $url);
     }
 }
