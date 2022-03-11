457165877c74a4dee7f76a0e424784750920f849
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-03-10 21:34:25 -0700

    Check if WP is installed before bootstraping

--- public/cms/wp-content/mu-plugins/lantern-bootstrap.php
+++ public/cms/wp-content/mu-plugins/lantern-bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
-define('LANTERN_URL', plugin_dir_url(__FILE__) . 'lantern');
-define('LANTERN_PATH', plugin_dir_path(__FILE__) . 'lantern');
+if (!wp_installing()) {
+    define('LANTERN_URL', plugin_dir_url(__FILE__) . 'lantern');
+    define('LANTERN_PATH', plugin_dir_path(__FILE__) . 'lantern');
 
-require_once __DIR__ . '/lantern/bootstrap.php';
+    require_once __DIR__ . '/lantern/bootstrap.php';
+}
