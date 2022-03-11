42eda79ecac4653c9b8a264817012eec3bba55c5
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-03-10 21:09:28 -0700

    Fix admin rest api issue

--- public/cms/wp-content/mu-plugins/lantern/src/Setup.php
+++ public/cms/wp-content/mu-plugins/lantern/src/Setup.php
@@ -7,6 +7,7 @@ class Setup
     public static function init(): void
     {
         add_filter('wp_using_themes', '__return_false');
+        add_filter('rest_url', [Setup::class, 'restUrl']);
 
         add_action('init', [Setup::class, 'redirect']);
     }
@@ -22,5 +23,20 @@ class Setup
         }
 
         return redirect('/wp-admin')->send();
+    }
+
+    /**
+     * Fixes WordPress REST API issue when
+     * WordPress Address (URL) is not the same as Site Address (URL)
+     */
+    public static function restUrl($url): string
+    {
+        if (!is_admin()) {
+            return $url;
+        }
+
+        $url = str_replace(home_url(), site_url(), $url);
+
+        return $url;
     }
 }
